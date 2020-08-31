f3f7a51ad8d5a94ee163cae4cfa83c7f707a8930
Harsh <gurjarhariom3114@gmil.com>
2020-09-01 03:02:23 +0530

    route files

---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+
+// Route::get('/items', 'ItemController@all')->name('items.all');
+
+// Route::post('/items', 'ItemController@store')->name('items.store');
+
+// Route::get('/items{items}', 'ItemController@show')->name('items.show');
+
+// Route::put('/items/{items}', 'ExpenseController@update')->name('expenses.update');
+
+// Route::delete('/items/{items}', 'ExpenseController@destory')->name('expenses.destroy');
